c724a92845615dd4b634b1675f22ed32c75ad2b6
Victoria Parry <vlparry@gmail.com>
2022-09-06 23:19:10 -0600

    setting up first fetch

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Eat Good Food!
+Looking for some inspiration in the kitchen? Look no further. Eat Good Food is chock full of healthy recipes to nourish your body!
 
-## 
+## Requirements
 
-### 
+## Installation
 
-### 
+## License  
 
 
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 
 const Recipes = () => {
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/recipes")
+      .then((r) => r.json())
+      .then((data) => {
+        setRecipes(data);
+      });
+  }, []);
+
   return (
-    <div>Recipes</div>
+    <div>
+      <ul>
+        {recipes.map(recipe => (
+          <li key={recipe.id}>
+            {recipe.title} {recipe.image}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
