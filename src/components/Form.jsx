5b2bb908cda83567908b091d571fd604f2c223c7
Victoria Parry <vlparry@gmail.com>
2022-09-16 12:26:45 -0600

    finishing form creation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -35,14 +35,56 @@ function handleSubmit(e){
     <section>
       <h1>Add a New Recipe Here:</h1>
       <form onSubmit={handleSubmit}>
+<p>
 <label>
   Recipe Name:
   <input 
   type="text"
+  placeholder='What is your recipe called?'
   name="title"
   value={newRecipes.title}
+  
   />
 </label>
+</p>
+<p>
+<label>
+  Ingredients:
+  <input 
+  type="text"
+  placeholder='What do we need to make your recipe?'
+  name="ingredients"
+  value={newRecipes.ingredients}
+  
+  />
+</label>
+</p>
+<p>
+<label>
+  Instructions:
+  <input 
+  type="text"
+  placeholder='How can we make this ourselves?'
+  name="instructions"
+  value={newRecipes.instructions}
+  
+  />
+</label>
+</p>
+<p>
+<label>
+  Image:
+  <input 
+  type="text"
+  placeholder='Do you have a picture of your food?'
+  name="image"
+  value={newRecipes.image}
+  
+  />
+</label>
+</p>
+
+
       </form>
     </section>
 
