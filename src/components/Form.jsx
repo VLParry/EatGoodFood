5b1c85c76964d7a5b2fd54f7634f79397eccce90
Victoria Parry <vlparry@gmail.com>
2022-09-16 12:02:38 -0600

    setting up POST request

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,9 +3,33 @@ import {useState, useEffect} from 'react';
 
 
 const Form = ({  }) => {
-const [newRecipes, setNewRecipes] = useState([])
+const [newRecipes, setNewRecipes] = useState({
+  title: "",
+  ingredients: "",
+  instructions: "",
+  image: "",
+  correctIndex: 0,
+})
 
+function handleSubmit(e){
+  e.preventDefault()
+  fetch("http://localhost:3000/recipes",{
+    method: "POST",
+    headers: {
+      "Content-Type" : "application/json"
+    },
+    body: JSON.stringify(
+      {
+        "title" : newRecipes.title,
+        "ingredients" : newRecipes.ingredients,
+        "instructions" : newRecipes.instructions,
+        "image" : newRecipes.image,
+        "correctIndex" : newRecipes.correctIndex
+      }
 
+    )
+  })
+}
   return (
     
 <div> </div>
